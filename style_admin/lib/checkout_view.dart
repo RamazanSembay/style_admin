707efa53abdafa59210fb548c23c0d7cefd7b671
Nurramazan Sembay <ramazansembai01@gmail.com>
2022-06-04 16:03:42 +0600

    style_admin

---
+++ style_admin/lib/checkout_view.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class CheckoutView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24), topRight: Radius.circular(24)),
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Тапсырыс берген: ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                    Text(
+                      'Арман',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.pink,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          ProductDivider(),
+          Container(
+            padding: EdgeInsets.only(left: 16, right: 16, bottom: 12),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(24),
+                    bottomRight: Radius.circular(24))),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.amber.shade50,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Icon(Icons.task_alt, color: Color(0xffFFDB53)),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  'Аль-Фараби 106',
+                  style: TextStyle(
+                    color: Color(0xff333333),
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Montserrat',
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    '240' + ' \u20b8',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+            width: 10,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  color: Colors.grey.shade200),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Flex(
+                    children: List.generate(
+                        (constraints.constrainWidth() / 10).floor(),
+                        (index) => SizedBox(
+                              height: 1,
+                              width: 5,
+                              child: DecoratedBox(
+                                decoration:
+                                    BoxDecoration(color: Colors.grey.shade400),
+                              ),
+                            )),
+                    direction: Axis.horizontal,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  );
+                },
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+            width: 10,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                  color: Colors.grey.shade200),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
